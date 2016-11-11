6588245406a53b21728d7b41fbe2ffda32a7f405
dreac0nic <spenser.m.bray@gmail.com>
2016-11-10 21:34:55 -0800

    Build a simple SFML test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-  cout << "Hello, rawrld!" << endl;
+  sf::RenderWindow window(sf::VideoMode(200, 200), "My Window");
+  sf::CircleShape shape(100.0f);
+  shape.setFillColor(sf::Color::Green);
+
+  // Main window loop, while window is open
+  while(window.isOpen()) {
+    sf::Event event;
+
+    while(window.pollEvent(event)) {
+      if(event.type == sf::Event::Closed) {
+	window.close();
+      }
+    }
+
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
   
   return 0;
 }

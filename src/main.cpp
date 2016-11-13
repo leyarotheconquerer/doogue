ca9b2229a0afb191ec3ef2f4e2163f615008465f
dreac0nic <spenser.m.bray@gmail.com>
2016-11-13 01:32:39 -0800

    Build a simple SFML demo with event handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <sstream>
+
 #include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "My Window");
+  stringstream buffer;
+
+  sf::Clock typingTimer;
+  
+  sf::RenderWindow window(sf::VideoMode(200, 200), "My Window", sf::Style::None);
   sf::CircleShape shape(100.0f);
   shape.setFillColor(sf::Color::Green);
 
@@ -14,9 +20,30 @@ int main(int argc, char* argv[])
     sf::Event event;
 
     while(window.pollEvent(event)) {
-      if(event.type == sf::Event::Closed) {
+      switch(event.type) {
+      case sf::Event::Closed:
 	window.close();
+	break;
+
+      case sf::Event::LostFocus:
+	cout << "Lost focus!" << endl;
+	break;
+
+      case sf::Event::GainedFocus:
+	cout << "Gained focus!" << endl;
+	break;
+
+      case sf::Event::TextEntered:
+	buffer << static_cast<char>(event.text.unicode);
+	typingTimer.restart();
+	
+	break;
       }
+    }
+
+    if(buffer.str().length() > 0 && typingTimer.getElapsedTime().asSeconds() > 1.0f) {
+      cout << "Typed: " << buffer.str() << endl;
+      buffer.str("");
     }
 
     window.clear();

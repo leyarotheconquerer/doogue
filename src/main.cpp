d6e8b43a290dbe4fed451adede87ce22423e03f9
dreac0nic <spenser.m.bray@gmail.com>
2016-11-17 00:40:44 -0800

    Some basic vector manipulation scripts + player movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
+#include <stdlib.h>
+#include <math.h>
+
 #include <iostream>
 #include <sstream>
 
 #include <SFML/Graphics.hpp>
 
+#define PLAYER_ACCEL 42.0f
+#define PLAYER_FRICTION 28.0f
+#define PLAYER_MAXSPEED 8.0f
+
 using namespace std;
+
+float magnitude(sf::Vector2f vector);
+float sqrMagnitude(sf::Vector2f vector);
 
 int main(int argc, char* argv[])
 {
@@ -14,6 +24,8 @@ int main(int argc, char* argv[])
   sf::RenderWindow window(sf::VideoMode(200, 200), "My Window", sf::Style::None);
 
   // Player Setup
+  sf::Vector2f player_speed(0.0f, 0.0f);
+  sf::Vector2f player_position(0.0f, 0.0f);
   sf::CircleShape player_shape(4.0f);
   player_shape.setFillColor(sf::Color::Red);
 
@@ -36,6 +48,7 @@ int main(int argc, char* argv[])
 	paused = false;
 	cout << "Resumed..." << endl;
 	break;
+      }
     }
       
     // Game time!!
@@ -43,13 +56,64 @@ int main(int argc, char* argv[])
 
     if(!paused) {
       // Game logic
+
+      // Player input
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+	player_speed.y += PLAYER_ACCEL*deltaTime.asSeconds();
+      }
+
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+	player_speed.y -= PLAYER_ACCEL*deltaTime.asSeconds();
+      }
+
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+	player_speed.x += PLAYER_ACCEL*deltaTime.asSeconds();
+      }
+
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+	player_speed.x -= PLAYER_ACCEL*deltaTime.asSeconds();
+      }
+
+      // Player physics
+      if(sqrMagnitude(player_speed) > 0.0f) {
+	float speed_magnitude = magnitude(player_speed);
+
+	// Apply friction
+	if(speed_magnitude <= PLAYER_FRICTION*deltaTime.asSeconds()) {
+	  player_speed.x = 0.0f;
+	  player_speed.y = 0.0f;
+	} else {
+	  player_speed -= (player_speed/speed_magnitude)*PLAYER_FRICTION*deltaTime.asSeconds();
+	}
+
+	// Cap player speed
+	// TODO: Replace with proper mathematical approach code
+	if(speed_magnitude > PLAYER_MAXSPEED) {
+	  player_speed = PLAYER_MAXSPEED*(player_speed/speed_magnitude)*deltaTime.asSeconds();
+	}
+	
+	// Apply player speed
+	player_position += player_speed*deltaTime.asSeconds();
+
+	cout << player_position.x << ", " << player_position.y << ":" << player_speed.x << ", " << player_speed.y << endl;
+      }
     }
 
     // Render game
     window.clear();
-    window.draw(shape);
+    window.draw(player_shape);
     window.display();
   }
   
   return 0;
+}
+
+float magnitude(sf::Vector2f vector)
+{
+  return sqrt(vector.x*vector.x + vector.y*vector.y);
+}
+
+float sqrMagnitude(sf::Vector2f vector)
+{
+  return vector.x*vector.x + vector.y*vector.y;
 }
